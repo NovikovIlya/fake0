33f46554a28ee068236b9974d35bb6a0df6781a0
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-01-13 12:16:02 +0300

    add2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const div = document.createElement('div')
 const header = document.createElement('h1');
 header.innerText = 'learn!!!!'
+console.log('ssss')
 
 div.appendChild(header)
 document.body.appendChild(div)
